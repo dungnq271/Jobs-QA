663a34fdeead1c58381eaff3c796c10e701bdbc1
dungnq271 <dungnguyen2712002@gmail.com>
2024-03-13 01:03:54 +0700

    Format using black

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
-    MessagesPlaceholder
+    MessagesPlaceholder,
 )
 
 import asyncio
@@ -21,25 +21,27 @@ from utils import *
 
 st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
 
-if 'responses' not in st.session_state:
-    st.session_state['responses'] = ["How can I assist you?"]
+if "responses" not in st.session_state:
+    st.session_state["responses"] = ["How can I assist you?"]
 
-if 'requests' not in st.session_state:
-    st.session_state['requests'] = []
+if "requests" not in st.session_state:
+    st.session_state["requests"] = []
 
-if 'documents' not in st.session_state:
-    st.session_state['documents'] = []    
+if "documents" not in st.session_state:
+    st.session_state["documents"] = []
 
 
-if 'buffer_memory' not in st.session_state:
-    st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+if "buffer_memory" not in st.session_state:
+    st.session_state.buffer_memory = ConversationBufferWindowMemory(
+        k=3, return_messages=True
+    )
 
 
 ## Create model
 # llm = OpenAI(temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 # system_msg_template = SystemMessagePromptTemplate.from_template(
-#     template="""Answer the question as truthfully as possible using the provided context, 
+#     template="""Answer the question as truthfully as possible using the provided context,
 #     and if the answer is not contained within the text below, say 'I don't know'""")
 
 
@@ -58,10 +60,14 @@ if not osp.exists(doc_dir):
 
 with st.sidebar:
     st.header("File Upload Options")
-    uploaded_files = st.file_uploader("Upload files", type=["txt", "csv", "pdf"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader(
+        "Upload files", type=["txt", "csv", "pdf"], accept_multiple_files=True
+    )
 
 
-async def parse_file(uploaded_file, existing_files=st.session_state["documents"]):
+async def parse_file(
+    uploaded_file, existing_files=st.session_state["documents"]
+):
     new_files = []
     filename = uploaded_file.name
     if filename not in existing_files:
@@ -70,15 +76,19 @@ async def parse_file(uploaded_file, existing_files=st.session_state["documents"]
         existing_files.append(filename)
         requests.put(
             "http://127.0.0.1:8000/update_text/",
-            json={"textlist": [filepath]},            
+            json={"text": filepath},
         )
         new_files.append(filename)
     return new_files
 
 
 async def parse_job():
-    new_files = await asyncio.gather(*[parse_file(uf, st.session_state["documents"])
-                                       for uf in uploaded_files])
+    new_files = await asyncio.gather(
+        *[
+            parse_file(uf, st.session_state["documents"])
+            for uf in uploaded_files
+        ]
+    )
     st.session_state["documents"].extend(new_files)
 
 
@@ -90,7 +100,7 @@ textcontainer = st.container()
 
 def on_click():
     st.session_state.user_input = ""
-    
+
 
 with textcontainer:
     query = st.text_input("Query: ", key="user_input")
@@ -103,7 +113,7 @@ with textcontainer:
             # st.write(refined_query)
             # context = find_match(refined_query)
             # context = find_match(query)
-            # print(context)  
+            # print(context)
             # response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
             response = get_response(query)
 
@@ -114,12 +124,15 @@ with textcontainer:
 
 
 with response_container:
-    if st.session_state['responses']:
-        for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i],key=str(i))
-            if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
-
+    if st.session_state["responses"]:
+        for i in range(len(st.session_state["responses"])):
+            message(st.session_state["responses"][i], key=str(i))
+            if i < len(st.session_state["requests"]):
+                message(
+                    st.session_state["requests"][i],
+                    is_user=True,
+                    key=str(i) + "_user",
+                )
 
 
 asyncio.run(parse_job())
