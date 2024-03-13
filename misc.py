795b52146d9098a428e40d317634399c5742199e
dungnq271 <dungnguyen2712002@gmail.com>
2024-03-14 00:03:30 +0700

    Add file description

---
+++ misc.py
@@ -0,0 +1,6 @@
+file_description = {
+    "bootstrap": "",
+    "uber_10q_march_2022": "containing financial information about UBER TECHNOLOGIES, INC.",
+    "allPlayersLookup": "containing biographical information on NHL hockey players",
+    "all_teams": "containing stats related to NHL hockey teams"
+}
