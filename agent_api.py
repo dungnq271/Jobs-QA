8958945d59e18f4c71a619cd7a61041d0ce4e2f4
dungnq271 <dungnguyen2712002@gmail.com>
2024-03-14 00:02:20 +0700

    Add asynccontextmanager for api lifespan

--- agent_api.py
+++ agent_api.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from contextlib import asynccontextmanager
 
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
@@ -17,7 +18,7 @@ from llama_index.core import (
 from llama_parse import LlamaParse
 from llama_index.vector_stores.astra import AstraDBVectorStore
 from llama_index.core.node_parser import MarkdownElementNodeParser
-from llama_index.core import StorageContext, SQLDatabase
+from llama_index.core import SQLDatabase
 from llama_index.core.postprocessor import SimilarityPostprocessor
 
 from llama_index.core.query_engine import NLSQLTableQueryEngine
@@ -27,16 +28,26 @@ from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 import pandas as pd
+from astrapy.db import AstraDB
 
 from utils import calculate_time
+from misc import file_description
 
 
 nest_asyncio.apply()
 
 
+#### Hyperparams ####
+table_name = "rag_demo"
+mode = "advanced"
+model = "gpt-3.5-turbo"
+file_desc = False
+post_delete_index = True
+
+
+#### Main app ####
 _ = load_dotenv(find_dotenv())  # read local .env file
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
 
 embed_model = OpenAIEmbedding(
     model="text-embedding-3-small",
@@ -46,14 +57,11 @@ embed_model = OpenAIEmbedding(
     max_tries=3,
 )
 
-llm = OpenAI(model="gpt-3.5-turbo")
+llm = OpenAI(model=model)
 
 Settings.llm = llm
 Settings.embed_model = embed_model
 Settings.chunk_size = 512
-
-
-app = FastAPI()
 
 
 class TextInput(BaseModel):
@@ -68,6 +76,7 @@ class Agent:
 
     tools = []
     engine = create_engine("sqlite:///:memory:", future=True)
+    text_filenames = []
     bootstrap_tool_name = "bootstrap"
     
     def __init__(
@@ -90,7 +99,7 @@ class Agent:
         self.node_parser = node_parser
         self.reranker = reranker
         self._load_index()
-        self._add_query_engine()
+        self._add_query_engine(self.bootstrap_tool_name)
         self._get_agent()
 
     def _load_index(self):
@@ -115,7 +124,7 @@ class Agent:
             verbose=True,
             # context=context,
             system_prompt=""" 
-            You are an agent designed to answer queries about structured-tables.
+            You are an agent designed to answer queries about structured tables.
             Please ALWAYS use the tools provided to answer a question. Do not rely on prior knowledge.
             If there is no information please answer you don't have that information.
             """,
@@ -123,7 +132,7 @@ class Agent:
 
     @staticmethod
     def add_df_to_sql_database(table_name: str, pandas_df: pd.DataFrame, engine: Engine) -> None:
-      pandas_df.to_sql(table_name, engine)
+        pandas_df.to_sql(table_name, engine)
 
     @calculate_time
     def parse_file(self, filepath):
@@ -134,8 +143,9 @@ class Agent:
             document = SimpleDirectoryReader(
                 input_files=[filepath], file_extractor={".pdf": self.parser}
             ).load_data()
+            filename = self._add_text_file(filepath)
             self._parse_document(document)
-            self._add_query_engine()
+            self._add_query_engine(filename)
 
         self._get_agent()
         print(self.tools)
@@ -170,6 +180,12 @@ class Agent:
         self.add_df_to_sql_database(table_name, df, self.engine)
         return table_name
 
+    def _add_text_file(self, filepath):
+        filename = osp.basename(filepath)
+        filename = osp.splitext(filename)[0]
+        self.text_filenames.append(filename)
+        return filename
+
     def _add_sql_engine(self, table_name):
         sql_tool = QueryEngineTool(
             query_engine=NLSQLTableQueryEngine(
@@ -178,14 +194,18 @@ class Agent:
             metadata=ToolMetadata(
                 name=f"sql_{table_name}",
                 description=(
-                    "Useful for translating a natural language query into an SQL query over tables"
-                    f" containing information about {table_name}."
+                    "Useful for translating a natural language query into an SQL query over table"
+                    f"{file_description[table_name]}"
                 ),
             ),
         )
         self.tools.append(sql_tool)
 
-    def _add_query_engine(self):
+    def _add_query_engine(self, filename):
+        if file_desc:
+            desc = file_description[filename]
+        else:
+            desc = ' '.join(filename.split('-')) if '-' in filename else filename
         self.query_engine = self.index.as_query_engine(
             similarity_top_k=15,
             node_postprocessors=[self.reranker],
@@ -194,17 +214,17 @@ class Agent:
         query_tool = QueryEngineTool(
             query_engine=self.query_engine,
             metadata=ToolMetadata(
-                name=self.bootstrap_tool_name,
+                name=f"query_{filename}",
                 description=(
-                    f"Useful for querying for information about text documents"
+                    "Useful for querying for information"
+                    f"from text documents about {desc}"
                 ),
             ),
         )
-        # self.tools.append(query_tool)
-        if len(self.tools) == 0:
-            self.tools.append(query_tool)
-        else:
-            self.tools[0] = query_tool
+        if len(self.tools) > 0:
+            if self.tools[0].metadata.name == self.bootstrap_tool_name:
+                self.tools.pop(0)
+        self.tools.append(query_tool)
 
     @calculate_time
     def chat(self, prompt: str):
@@ -218,24 +238,46 @@ class Agent:
         return response
 
 
-engine = Agent(
-    mode="advanced",
-    collection_name="rag_demo",
-    node_parser=MarkdownElementNodeParser(
-        llm=OpenAI(
-            model="gpt-3.5-turbo-0125",
+def delete_table():
+    # Drop the table created for this session
+    db = AstraDB(
+        token=os.getenv("ASTRA_TOKEN"),
+        api_endpoint=os.getenv("ASTRA_API_ENDPOINT"),
+        namespace=os.getenv("ASTRA_NAMESPACE"),
+    )
+    db.delete_collection(collection_name=table_name)
+    print("----------------------APP EXITED----------------------")
+
+
+agents = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the agent
+    agents["answer_to_everything"] = Agent(
+        mode=mode,
+        collection_name=table_name,
+        node_parser=MarkdownElementNodeParser(
+            llm=llm,
+            num_workers=4,
         ),
-        num_workers=4,
-    ),
-    reranker=SimilarityPostprocessor(similarity_cutoff=0.5),
-)
+        reranker=SimilarityPostprocessor(similarity_cutoff=0.5),
+    )
+    yield
+    # Clean up the data resources    
+    if post_delete_index:
+        delete_table()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/query_from_text/", status_code=200)
 async def query_from_text(prompt: TextInput):
-    return engine.chat(prompt.text)
+    return agents["answer_to_everything"].chat(prompt.text)
 
 
 @app.put("/update_text/", status_code=200)
 async def update_text(filepath: TextInput):
-    engine.parse_file(filepath.text)
+    agents["answer_to_everything"].parse_file(filepath.text)
