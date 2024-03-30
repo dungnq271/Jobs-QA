e7634074ab8e8bd77e5d92bdba08eabbfca5a7a4
dungnq271 <dungnguyen2712002@gmail.com>
2024-03-31 00:45:36 +0700

    Add Claude, FnrAgent, RouterRetriever

--- experimental/table/llama_index_sql_with_index.py
+++ experimental/table/llama_index_sql_with_index.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
 
 # %%
 from llama_index.llms.openai import OpenAI
+from llama_index.llms.anthropic import Anthropic
 from llama_index.core import (
     Settings,
     VectorStoreIndex,
@@ -24,6 +26,8 @@ from llama_index.core.query_engine import (
     SQLAutoVectorQueryEngine
 )
 from llama_index.core.tools import QueryEngineTool
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Document
 
 # %%
 client = qdrant_client.QdrantClient(
@@ -41,6 +45,15 @@ vector_store = QdrantVectorStore(client=client, collection_name="jobs_posted")
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 vector_index = VectorStoreIndex([], storage_context=storage_context)
 
+# %%
+llm = OpenAI(model="gpt-3.5-turbo-instruct")
+llm = Anthropic(model="claude-3-sonnet")
+Settings.llm = llm
+Settings.tokenizer = Anthropic().tokenizer
+
+resp = llm.complete("Paul Graham is ")
+print(resp)
+
 # %% [markdown]
 ### Load data
 
@@ -56,7 +69,7 @@ def add_df_to_sql_database(table_name: str, pandas_df: pd.DataFrame, engine: Eng
 engine = create_engine("sqlite:///:memory:", future=True)
 
 # %%
-df = pd.read_csv("../../documents/job_vn_posted_full.csv")
+df = pd.read_csv("../../documents/job_vn_posted_full_recent_v2.csv")
 df.columns
 
 # %%
@@ -82,30 +95,58 @@ sql_query_engine = NLSQLTableQueryEngine(
 ### Build Vector Index¶
 
 # %%
+cols = df.columns.tolist()
+acc_cols = ["Posted", "Full / Part Time", "Salary", "Link"]
+
+# %%
+row = result[0][-2]
+print(row)
+
+# %%
+node_parser = SentenceSplitter.from_defaults(chunk_size=50, chunk_overlap=10)
+nodes = node_parser.get_nodes_from_documents([Document(text=row)])
+
+# %%
+for i, x in enumerate(nodes):
+    print(f"Chunk {i}:", x.text)
+
+# %%
+len(cols), len(result[0][2:-2])
+
+# %%
 nodes = []
 
 for row in result:
-    company = row[3]
-    link = row[-1]
-    desc = str(row[2:-1])
-    node = TextNode(text=desc)
-    node.metadata = {"title": company, "link": link}
-    nodes.append(node)
+    desc = row[-2]
+    sub_nodes = node_parser.get_nodes_from_documents([Document(text=desc)])
+    for node in sub_nodes:
+        node.metadata = {c: d for c, d in zip(cols, row[1:]) if c in acc_cols}
+    # print(desc)
+    # print(node.metadata)
+    # break
+    nodes.extend(sub_nodes)
 
+# %%
 vector_index.insert_nodes(nodes)
 
 # %%
-llm = OpenAI(model="gpt-3.5-turbo")
-Settings.llm = llm
+len(nodes)
 
+# %%
 vector_store_info = VectorStoreInfo(
     content_info="AI Engineer job information of different companies",
     metadata_info=[
         MetadataInfo(
-            name="company", type="str", description="The name of the company"
+            name="Posted", type="str", description="How many days ago was the job posted"
         ),
         MetadataInfo(
-            name="link", type="str", description="Link to the posted job"
+            name="Full / Part Time", type="str", description="Working time for the job"
+        ),                
+        MetadataInfo(
+            name="Salary", type="str", description="Job's pay range"
+        ),
+        MetadataInfo(
+            name="Link", type="str", description="Link to the posted job"
         ),        
     ],
 )
@@ -127,6 +168,7 @@ sql_tool = QueryEngineTool.from_defaults(
     description=(
         f"Useful for translating a natural language query into a SQL query over"
         f" a table containing: {', '.join(df.columns.to_list()[1:-1])}"
+        f" with both Vietnamese and English texts"
     ),
 )
 vector_tool = QueryEngineTool.from_defaults(
@@ -137,21 +179,109 @@ vector_tool = QueryEngineTool.from_defaults(
     ),
 )
 
-query_engine = SQLAutoVectorQueryEngine(
+# %%
+query_1 = "List me all the links to jobs which include both NLP and Computer Vision"
+# query = "List me the salary of the top 3 most recent posted jobs?"
+# query = "List me the top skills in demand for all the jobs?"
+query_2 = "List me all the links to jobs which demand only 1 year of experience"
+
+# %% [markdown]
+### Using SQLAutoVectorQueryEngine
+
+# %%
+sqlauto_engine = SQLAutoVectorQueryEngine(
     sql_tool, vector_tool, llm=llm
 )
 
 # %%
-query = "List me all the links to jobs which include both NLP and Computer Vision"
-# query = "List me the salary of the top 3 most recent posted jobs?"
-# query = "List me the top skills in demand for all the jobs?"
-response = query_engine.query(query)
+query = query_2
+response = sqlauto_engine.query(query)
+print(response)
+
+# %%
+response.__dict__
+
+# %% [markdown]
+### Using ReActAgent
+from llama_index.core.agent import ReActAgent
+
+# %%
+system_prompt = \
+""" 
+You are an agent designed to answer queries from user.
+Please ALWAYS use the tools provided to answer a question. Do not rely on prior knowledge.
+If there is no information please answer you don't have that information.
+"""
+
+# %%
+react_agent = ReActAgent.from_tools(
+    [sql_tool, vector_tool],
+    llm=llm,
+    verbose=True,
+    system_prompt=system_prompt,
+    enable_hybrid=True
+)            
+
+# %%
+query = query_1
+response = react_agent.chat(query)
+print(response)
+
+# %%
+response = react_agent.chat("Yes")
+print(response)
+
+# %%
+if "sql_query" in response.sources[0].raw_output.metadata:
+    print(response.sources[0].raw_output.metadata["sql_query"])
+
+# %%
+response.source_nodes
+
+# %% [markdown]
+### Using Retriever Router Query Engine¶
+from llama_index.core import VectorStoreIndex
+from llama_index.core.objects import ObjectIndex, SimpleToolNodeMapping
+from llama_index.core.query_engine import ToolRetrieverRouterQueryEngine
+
+# %%
+tool_mapping = SimpleToolNodeMapping.from_objects([sql_tool, vector_tool])
+obj_index = ObjectIndex.from_objects(
+    [sql_tool, vector_tool],
+    tool_mapping,
+    VectorStoreIndex,
+)
+
+# %%
+router_engine = ToolRetrieverRouterQueryEngine(
+    obj_index.as_retriever(),
+)
+
+# %%
+print(query)
+response = router_engine.query(query)
 print(response)
 
 # %%
 response.source_nodes
 
+# %% [markdown]
+### Using FnRetrieverOpenAIAgent
+from llama_index.agent.openai_legacy import FnRetrieverOpenAIAgent
+
 # %%
+fnr_agent = FnRetrieverOpenAIAgent.from_retriever(
+    obj_index.as_retriever(similarity_top_k=3),
+    system_prompt=system_prompt,
+    verbose=True
+)
+
+# %%
+response = fnr_agent.query(query)
+print(response)
+
+# %% [markdown]
+### Test the result
 with engine.connect() as conn:
     cursor = conn.execute(
         text(
@@ -164,5 +294,22 @@ with engine.connect() as conn:
 # %%
 from IPython.display import display, Markdown
 display(Markdown(result[0][0]))
+
+# %%
+with engine.connect() as conn:
+    cursor = conn.execute(
+        text(
+        "SELECT Company, Link FROM jobPosted"
+        " WHERE Description LIKE '%1 năm kinh nghiệm%'"
+        )
+    )
+    result = cursor.fetchall()
+
+# %%
+len(result)
+
+# %%
+for res in result:
+    print(res)
 
 # %%
