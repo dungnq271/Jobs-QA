a13d61c44da18214699e83b71975e6f7a81daf30
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-13 17:54:30 +0700

    Add base scraper

---
+++ src/scraper/base_scraper.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class BaseScraper(ABC):
+
+    def __init__(
+        self,
+        output_dpath: str,
+        top_recent: Optional[int] = None,
+    ):
+        self.output_dpath = output_dpath
+        self.top_recent = top_recent
+
+    @abstractmethod
+    def scrape(self, url: str, name: str):
+        """Start scraping process."""
