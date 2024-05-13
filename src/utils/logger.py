7e348dcf73c0f98fbf3085f5f20ed31658e1d918
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-13 18:07:56 +0700

    Add logger utils

---
+++ src/utils/logger.py
@@ -0,0 +1,35 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import yaml  # type: ignore
+
+from .io import create_dir
+
+
+def setup_logging(log_dir, config_fpath):
+    create_dir(log_dir)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    log_config = Path(config_fpath)
+    if log_config.is_file():
+        with open(log_config, "r") as f:
+            config = yaml.safe_load(f.read())
+            # modify logging paths based on log_dir
+            for __, handler in config["handlers"].items():
+                if "filename" in handler:
+                    handler["filename"] = "/".join(
+                        [log_dir, handler["filename"]]
+                    )
+
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        print(
+            f"Warning: logging configuration file is "
+            f"not found in {log_config}."
+        )
+
+
+def get_logger(name):
+    return logging.getLogger(name)
