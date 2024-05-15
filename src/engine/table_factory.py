1d788da2515d9eed499bb6fa649a4b2df39f483a
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-15 17:43:36 +0700

    Rewrite some function

--- src/engine/table_factory.py
+++ src/engine/table_factory.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional
+from typing import Any
 
 from llama_index.core import SQLDatabase, VectorStoreIndex
 from llama_index.core.query_engine import (  # SQLAutoVectorQueryEngine,
@@ -6,7 +6,8 @@ from llama_index.core.query_engine import (  # SQLAutoVectorQueryEngine,
     RetrieverQueryEngine,
     SQLJoinQueryEngine,
 )
-from llama_index.core.retrievers import RecursiveRetriever
+from llama_index.core.retrievers import BaseRetriever, RecursiveRetriever
+from llama_index.core.schema import BaseNode
 from llama_index.core.tools import QueryEngineTool
 
 from src.prompt import QUERY_SQL_DESCRIPTION, QUERY_TEXT_DESCRIPTION
@@ -20,34 +21,42 @@ class TableEngineFactory(BaseFactory):
         self,
         db_engine: Any,
         vector_store_index: VectorStoreIndex,
-        node_postprocessors: Optional[List[Any]] = None,
+        node_postprocessors: list[Any] | None = None,
     ):
         super().__init__(vector_store_index=vector_store_index)
         self._logger = get_logger(__name__)
         self._db_engine = db_engine
         self._node_postprocessors = node_postprocessors
 
-    def get_retriever_engine(self, id_node_mapping, **kwargs):
+    def get_retriever(self, **kwargs):
         """Get retriever engine."""
 
         self._logger.info("Get retriever engine...")
-        vector_retriever = self._vector_store_index.as_retriever(
-            similarity_top_k=3
-        )
-        retriever_engine = RecursiveRetriever(
+        retriever = self._vector_store_index.as_retriever(similarity_top_k=3)
+        return retriever
+
+    def get_recursive_retriever(
+        self,
+        retriever: BaseRetriever,
+        id_node_mapping: dict[str, BaseNode],
+        **kwargs,
+    ):
+        """Get recursive retriever engine."""
+
+        self._logger.info("Get recursive retriever engine...")
+        retriever = RecursiveRetriever(
             "vector",
-            retriever_dict={"vector": vector_retriever},
-            query_engine_dict=id_node_mapping,
+            retriever_dict={"vector": retriever},
+            node_dict=id_node_mapping,
             verbose=True,
         )
+        return retriever
 
-        return retriever_engine
-
-    def get_query_engine(self, retriever_engine, **kwargs):
+    def get_query_engine(self, retriever: BaseRetriever, **kwargs):
         """Get query engine."""
         self._logger.info("Get query engine...")
         query_engine = RetrieverQueryEngine.from_args(
-            retriever_engine,
+            retriever,
             node_postprocessors=self._node_postprocessors,
         )
         return query_engine
@@ -66,16 +75,17 @@ class TableEngineFactory(BaseFactory):
     def get_qa_engine(self, metadata, id_node_mapping, **kwargs):
         """Get table query engine for the recently uploaded table file."""
         # Get vector index query engine
-        recursive_retriever = self.get_retriever_engine(
-            id_node_mapping=id_node_mapping
+        vector_retriever = self.get_retriever()
+        recursive_retriever = self.get_recursive_retriever(
+            retriever=vector_retriever, id_node_mapping=id_node_mapping
         )
-        retriever_query_engine = self.get_query_engine(recursive_retriever)
+        query_engine = self.get_query_engine(retriever=recursive_retriever)
 
         query_desc = QUERY_TEXT_DESCRIPTION.format(
             file_description=metadata["file_description"]
         )
         vector_tool = QueryEngineTool.from_defaults(
-            query_engine=retriever_query_engine, description=query_desc
+            query_engine=query_engine, description=query_desc
         )
 
         # Get SQL query engine
