b5d53fde439dc2399912fb778097170fe75178d9
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-24 00:59:30 +0700

    Add some requests functions

--- src/utils/api.py
+++ src/utils/api.py
@@ -3,23 +3,43 @@ import time
 import requests  # type: ignore
 
 
-def get_response(input):
+def get_response(input: str):
     response = requests.post(
-        url="http://127.0.0.1:8000/chat",
+        url="http://127.0.0.1:8000/agent/chat",
         json={"text": input},
     )
     assert response.status_code == 200, response.status_code
     json_response = response.json()
     if isinstance(json_response, dict):
-        response = json_response["response"]
+        response_str = json_response["response"]
     elif isinstance(json_response, str):
-        response = json_response
+        response_str = json_response
     else:
         raise TypeError("Response must be of type str or Dict")
-    return response
+    return response_str
 
 
-def response_generator(response):
+def response_generator(response: str):
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
+
+
+def update_llm(model_name: str):
+    requests.put(
+        "http://127.0.0.1:8000/agent/update_llm",
+        json={"text": model_name},
+    )
+
+
+def update_table(file_path: str):
+    requests.put(
+        "http://127.0.0.1:8000/document/update_table",
+        json={"text": file_path},
+    )
+
+
+def get_path():
+    response = requests.post("http://127.0.0.1:8000/document/get_path")
+    assert response.status_code == 200, response.status_code
+    return response.json()
