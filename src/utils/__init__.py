571cc56afb488725f9c01a2627531f52f5a2e8b0
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-24 01:00:02 +0700

    Move a line to a new place

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,9 @@
 from .api import get_response, response_generator
 from .debug import debug_subquestion, debug_table_qa
+from .func import calculate_time, modify_days_to_3digits
 from .io import create_dir, get_config, save_file
 from .logger import get_logger, setup_logging
 from .meta import CHUNKING_REGEX, metadata
-from .func import calculate_time, modify_days_to_3digits
 
 __all__ = [
     "get_response",
