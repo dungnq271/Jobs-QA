0423a4e56c96ed5cd99db0acccd8e663d41deb5a
dungnq271 <dungnguyen2712002@gmail.com>
2024-05-24 01:07:03 +0700

    Reformat the file

--- src/engine/utils.py
+++ src/engine/utils.py
@@ -4,6 +4,5 @@ from llama_index.core.schema import IndexNode, TransformComponent
 class TextLinkToSource(TransformComponent):
     def __call__(self, nodes, **kwargs):
         return [
-            IndexNode.from_text_node(node, node.source_node.node_id)
-            for node in nodes
+            IndexNode.from_text_node(node, node.source_node.node_id) for node in nodes
         ]
