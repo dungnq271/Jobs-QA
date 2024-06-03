5f736cc48f47179e60fb59fff497ed512d69508b
dungnq271 <dungnguyen2712002@gmail.com>
2024-06-03 11:19:46 +0700

    Update all database files

--- src/database/base_database.py
+++ src/database/base_database.py
@@ -1,60 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 from llama_index.core import VectorStoreIndex
-from llama_index.core.schema import Document, TransformComponent
-from llama_index.core.service_context import ServiceContext
-from llama_index.vector_stores.qdrant import QdrantVectorStore
-from qdrant_client import QdrantClient
 
 
 class BaseDatabase(ABC):
     """Base database builder."""
 
-    def __init__(
-        self,
-        collection_name: str,
-        client: QdrantClient | None = None,
-        url: str | None = None,
-        service_context: ServiceContext | None = None,
-        transformations: list[TransformComponent] | None = None,
-        enable_hybrid=False,
-    ):
+    @abstractmethod
+    def __init__(self):
         """Initial database."""
-        self._client = client
-        self._collection_name = collection_name
-        self._service_context = service_context
-        self._transformations = transformations
 
-        if client is None and url is not None:
-            self._client = QdrantClient(url=url)
-        elif client is not None:
-            self._client = client
-        else:
-            raise AssertionError("Both url and client cannot be None")
-
-        self._vector_store = QdrantVectorStore(
-            collection_name=self._collection_name,
-            client=self._client,
-            enable_hybrid=enable_hybrid,
-        )
-        self._index = VectorStoreIndex.from_vector_store(
-            vector_store=self._vector_store,
-            service_context=self._service_context,
-            transformations=self._transformations,
-        )
-
-    def get_index(self):
-        """Get vector store index."""
-        return self._index
+    @property
+    @abstractmethod
+    def index(self) -> VectorStoreIndex:
+        """Get index of database"""
 
     @abstractmethod
-    def insert_documents(self, documents: list[Document]):
+    def insert_documents(self, documents: List):
         """Add document to the database."""
 
     @abstractmethod
-    def update_documents(self, documents: list[Document]):
-        """Update documents"""
-
-    @abstractmethod
-    def delete_documents(self, file_paths: list[str]):
+    def delete_documents(self, file_paths: List):
         """Delete document from the database."""
--- src/database/qdrant.py
+++ src/database/qdrant.py
@@ -1,75 +1,190 @@
-import os.path as osp
+import os
+import warnings
+from typing import Dict
 
-from llama_index.core.schema import Document, TextNode, TransformComponent
-from llama_index.core.service_context import ServiceContext
-from llama_index.core.storage.docstore.simple_docstore import (
-    SimpleDocumentStore,
-)
-from llama_index.core.storage.docstore.types import (
-    DEFAULT_PERSIST_FNAME as DOCSTORE_FNAME,
-)
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.core.schema import BaseNode
+from llama_index.core.storage.docstore import BaseDocumentStore, SimpleDocumentStore
+from llama_index.core.storage.index_store import SimpleIndexStore
 from qdrant_client import QdrantClient
 
-from src.utils import calculate_time, get_logger
-
-from .base_database import BaseDatabase
+from src.database.base_database import BaseDatabase
+from src.utils import calculate_time
+from src.utils.extension_class.extension_multimodal_vector_store_index import (
+    MultiModalVectorStoreIndexExtension,
+)
+from src.utils.extension_class.extension_qdrant_vector_store_index import (
+    QdrantVectorStoreExtension,
+)
+from src.utils.extension_class.extension_vector_store_index import (
+    VectorStoreIndexExtension,
+)
 
 
 class QdrantTextDatabase(BaseDatabase):
     def __init__(
         self,
-        collection_name: str,
-        client: QdrantClient | None = None,
-        url: str | None = None,
-        service_context: ServiceContext | None = None,
-        transformations: list[TransformComponent] | None = None,
-        enable_hybrid=True,
+        client: QdrantClient,
+        collection_name: str = "rag_demo",
+        enable_hybrid=False,
         persist_dir: str = "./storage",
+        **kwargs,
     ):
-        super().__init__(
-            client=client,
-            url=url,
+        self.client = client
+        self.collection_name = collection_name
+        self.persist_dir = persist_dir
+        self.vector_store = QdrantVectorStoreExtension(
             collection_name=collection_name,
-            service_context=service_context,
-            transformations=transformations,
+            client=client,
             enable_hybrid=enable_hybrid,
         )
-        self._logger = get_logger(__name__)
-        self._persist_dir = persist_dir
 
-    def insert_documents(self, documents: list[Document]):
+        if os.path.exists(self.persist_dir):
+            self.storage_context = StorageContext.from_defaults(
+                persist_dir=self.persist_dir, vector_store=self.vector_store
+            )
+            self.processor = load_index_from_storage(self.storage_context, **kwargs)
+        else:
+            self.storage_context = StorageContext.from_defaults(
+                docstore=SimpleDocumentStore(),
+                vector_store=self.vector_store,
+                index_store=SimpleIndexStore(),
+            )
+            self.processor = VectorStoreIndexExtension(
+                nodes=[], storage_context=self.storage_context, **kwargs
+            )
+
+    def exist_collection(self):
+        all_collections = self.client.get_collections()
+        all_name_collections = [
+            collection.name for collection in all_collections.collections
+        ]
+        exist_collection = self.collection_name in all_name_collections
+        return exist_collection
+
+    @property
+    def index(self) -> VectorStoreIndex:
+        """Get index of database"""
+        return self.processor
+
+    @property
+    def docstore(self) -> BaseDocumentStore:
+        return self.processor.docstore
+
+    @property
+    def id_node_mapping(self) -> Dict[str, BaseNode]:
+        return self.docstore.docs
+
+    def filter_extension(self, value: str, key: str):
+        """Filter a document and it's nodes by using value corresponding to key."""
+        return self.vector_store.filter_extension(value=value, key=key)
+
+    @calculate_time(name="Insert Documents")
+    def insert_documents(self, documents):
+        """Insert documents"""
+
+        file_path = documents[0].metadata["file_path"]
+
+        if (
+            not self.exist_collection()
+            or len(self.filter_extension(file_path, "file_path")[0]) == 0
+        ):
+            for document in documents:
+                if "file_path" not in document.metadata:
+                    raise Exception("Document doesn't include file_path in metadata !")
+                self.processor.insert(document)
+
+        else:
+            # do nothing
+            print(f"{file_path} already exists")
+            pass
+
+    def insert_documents_to_docstore(self, documents):
+        self.processor.docstore.add_documents(documents)
+
+    def persist(self):
+        self.storage_context.persist(persist_dir=self.persist_dir)
+
+    def update_documents(self, documents):
+        """Update documents"""
+        for document in documents:
+            # TODO: Need checkbox to ask customer
+            warnings.warn(
+                "Document is existed in the database. "
+                "We will overwrite the document!",
+            )
+            self.delete_documents(document.metadata["file_path"])
+            self.processor.insert(document)
+
+    def delete_documents(self, file_paths):
+        """Delete documents"""
+        for file_path in file_paths:
+            self.processor.delete_extension(value=file_path)
+
+
+class QdrantImageDatabase(BaseDatabase):
+    def __init__(self, client, collection_name, service_context, enable_hybrid=False):
+        self.client = client
+        self.collection_name = collection_name
+        self.service_context = service_context
+
+        self.text_store = QdrantVectorStoreExtension(
+            client=self.client,
+            collection_name=f"{collection_name}_text",
+            enable_hybrid=enable_hybrid,
+        )
+
+        self.image_store = QdrantVectorStoreExtension(
+            client=self.client,
+            collection_name=f"{collection_name}_image",
+            enable_hybrid=enable_hybrid,
+        )
+
+        self.storage_context = StorageContext.from_defaults(
+            vector_store=self.text_store, image_store=self.image_store
+        )
+        self.processor = MultiModalVectorStoreIndexExtension.from_storage_context(
+            storage_context=self.storage_context, service_context=self.service_context
+        )
+
+        all_collections = self.client.get_collections()
+        all_name_collections = [
+            collection.name for collection in all_collections.collections
+        ]
+
+        self.exist_collection = (
+            f"{self.collection_name}_text" in all_name_collections
+            and f"{self.collection_name}_image" in all_name_collections
+        )
+
+    def get_index(self):
+        """Get index of database"""
+        return self.processor
+
+    def insert_documents(self, documents):
         """Insert documents"""
         for document in documents:
             if "file_path" not in document.metadata:
                 raise Exception("Document doesn't include file_path in metadata !")
-            self._index.insert(document)
+            if not self.exist_collection:
+                self.processor.insert(document)
+                return
+            # Check document exist in the database
+            check_exist = self.processor.filter_extension(
+                document.metadata["file_path"]
+            )
+            if len(check_exist[0][0]) == 0 and len(check_exist[1][0]) == 0:
+                self.processor.insert(document)
+            else:
+                # TODO: Need checkbox to ask customer
+                warnings.warn(
+                    "Document is existed in the database. "
+                    "We will overwrite the document!"
+                )
+                self.delete_documents(document.metadata["file_path"])
+                self.processor.insert(document)
 
-    def update_documents(self, documents: list[Document]):
-        """Update documents"""
-        raise NotImplementedError
-
-    def delete_documents(self, file_paths: list[str]):
-        """Delete documents"""
-        raise NotImplementedError
-
-    @calculate_time
-    def preprocess(self, documents: list[Document], **kwargs):
-        """Preprocess the documents"""
-        self._logger.info("Preprocess the document...")
-        docstore_path = osp.join(self._persist_dir, DOCSTORE_FNAME)
-
-        if not osp.exists(self._persist_dir):
-            for document in documents:
-                self._index.insert(document)
-            self._docstore = self._index.docstore
-            self._docstore.add_documents(documents)
-            self._docstore.persist(persist_path=docstore_path)
-        else:
-            self._docstore = SimpleDocumentStore.from_persist_dir(self._persist_dir)
-
-        docstore_dict = self._docstore.to_dict()
-        id2nodes = {
-            id: TextNode.from_dict(node_dict["__data__"])
-            for id, node_dict in docstore_dict["docstore/data"].items()
-        }
-        return id2nodes
+    def delete_documents(self, file_paths):
+        """Delete document"""
+        for file_path in file_paths:
+            self.processor.delete_extension(value=file_path)
