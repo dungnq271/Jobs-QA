bbece9d70255d29f15328163edb6f1a0a7887126
dungnq271 <dungnguyen2712002@gmail.com>
2024-06-03 11:34:47 +0700

    Rewrite the whole app to chat with uploaded files

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
 import asyncio
-import os
-import os.path as osp
 
-import pandas as pd
 import streamlit as st
 
-from src.utils import api, get_config, save_file
-
-# import code
-# code.interact(local=locals())
-
-
-async def add_file(uploaded_file, existing_files):
-    new_files = []
-    file_name = uploaded_file.name
-    if file_name not in existing_files:
-        file_path = osp.join(doc_dir, osp.basename(file_name))
-        save_file(uploaded_file.read(), file_path)
-        existing_files.append(file_name)
-        api.update_table(file_path)
-        new_files.append(file_name)
-    return new_files
-
-
-async def add_file_process():
-    new_files = await asyncio.gather(
-        *[add_file(uf, st.session_state.documents) for uf in uploaded_files]
-    )
-    st.session_state.documents.extend(new_files)
-
+from src.utils import api, app, io
 
 if __name__ == "__main__":
     # Hyperparams
-    doc_dir = "./documents"
-    refine_query = False
-
-    st.set_page_config(layout="wide")
+    document_dir_path = "./documents"
 
     title = "Chatbot with LlamaIndex 🦙, ChatGPT, QdrantDB, and Streamlit"
     st.markdown(
-        f"<h2 style='text-align: center;'>{title}</h2>",
+        "<h2 style='text-align: center;'>" f"{title}</h2>",
         unsafe_allow_html=True,
     )
 
@@ -49,70 +20,92 @@ if __name__ == "__main__":
             {"role": "assistant", "content": "Hello. How can I assist you?"}
         ]
 
-    if "documents" not in st.session_state:
-        config = get_config("./config/scraper_config.yml")
-        st.session_state.documents = [
-            osp.join(config["output_dir"], config["name"] + ".csv")
-        ]
-
     if "llm" not in st.session_state:
         st.session_state.llm = "gpt-3.5-turbo"
 
+    if "documents" not in st.session_state:
+        st.session_state.documents = []
+
+    if "all_tools" not in st.session_state:
+        st.session_state.all_tools = []
+
+    if "use_tools" not in st.session_state:
+        st.session_state.use_tools = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
     # Upload and parse files
-    if not osp.exists(doc_dir):
-        os.makedirs(doc_dir, exist_ok=True)
+    io.create_dir(document_dir_path)
 
     with st.sidebar:
         st.header("Options")
+
+        st.subheader("Uploaded files")
         uploaded_files = st.file_uploader(
             "# Upload files",
-            type=["csv"],
+            type=["txt", "csv", "pdf", "pptx", "ppt"],
             accept_multiple_files=True,
             # label_visibility="hidden"
         )
-        model = st.selectbox(
+
+        st.subheader("Your model")
+        st.selectbox(
             "Choose your LLM",
             (
                 "gpt-3.5-turbo",
-                "claude-3-haiku-20240307",
-                "claude-3-sonnet-20240229",
+                # "claude-3-haiku-20240307",
+                # "claude-3-sonnet-20240229",
             ),
             key="llm",
             on_change=api.update_llm,
-            args=(st.session_state.llm),
+            args=(st.session_state.llm,),
         )
 
-    c1, c2 = st.columns((2, 1), gap="large")
+        st.subheader("Tools")
+        if not st.session_state.all_tools:  # Call only once
+            st.session_state.all_tools = api.get_list_api_tools()
 
-    with c1:
-        st.header("Jobs Data")
-        current_table = pd.read_csv(st.session_state.documents[-1])
-        st.dataframe(current_table)
+        for tool in st.session_state.all_tools:
+            check = st.checkbox(tool)
+            if check and tool not in st.session_state.use_tools:  # add tool
+                st.session_state.use_tools.append(tool)
+                asyncio.run(app.add_tools_process(tools=[tool]))
+            elif tool in st.session_state.use_tools and not check:  # remove tool
+                st.session_state.use_tools.remove(tool)
+                asyncio.run(app.remove_tools_process(tools=[tool]))
 
-    with c2:
-        st.header("Chat with Jobs Data")
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            response = api.get_response(prompt)
+            response_stream = st.write_stream(api.response_generator(response))
 
-        # Accept user input
-        if prompt := st.chat_input("What is up?"):
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            # Display user message in chat message container
-            with st.chat_message("user"):
-                st.markdown(prompt)
+        # Get agent latest tool call
+        tool_results = api.get_tool_call()
+        # for tool, results in tool_results.items():
+        if tool_results:
+            with st.expander("Tools Use"):
+                st.write(tool_results)
 
-            # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                response = api.get_response(prompt)
-                response_stream = st.write_stream(api.response_generator(response))
+        # Add assistant response to chat history
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response_stream}
+        )
 
-            # Add assistant response to chat history
-            st.session_state.messages.append(
-                {"role": "assistant", "content": response_stream}
-            )
-
-    asyncio.run(add_file_process())
+    asyncio.run(
+        app.add_documents_process(
+            document_dir_path=document_dir_path,
+            uploaded_files=uploaded_files,
+            documents=st.session_state.documents,
+        )
+    )
