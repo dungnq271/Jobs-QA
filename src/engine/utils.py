b7b986fef5d9161a44dbe7083350f8c5e88cee1c
dungnq271 <dungnguyen2712002@gmail.com>
2024-06-03 11:34:58 +0700

    Remove unnecessary files

--- src/engine/utils.py
+++
@@ -1,8 +0,0 @@
-from llama_index.core.schema import IndexNode, TransformComponent
-
-
-class TextLinkToSource(TransformComponent):
-    def __call__(self, nodes, **kwargs):
-        return [
-            IndexNode.from_text_node(node, node.source_node.node_id) for node in nodes
-        ]
